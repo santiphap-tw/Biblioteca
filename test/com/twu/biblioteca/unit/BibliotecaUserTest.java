4881272e089da7053a0e1aafacde8a70d5390bc3
IQ <santiphap.w@thoughtworks.com>
2020-02-03 14:35:12 +0700

    Change test style for BibliotecaUserTest

--- test/com/twu/biblioteca/unit/BibliotecaUserTest.java
+++ test/com/twu/biblioteca/unit/BibliotecaUserTest.java
@@ -19,55 +19,45 @@ public class BibliotecaUserTest {
     }
 
     @Test
-    public void getUsersTest() {
-        // Getter method
-    }
-
-    @Test
-    public void getCurrentUserTest() {
-        // Getter method
-    }
-
-    @Test
-    public void loginTest() {
-        // Positive Test
+    public void shouldLoginCorrectUser() {
         // When
         User user = bibliotecaUser.login("111-1111", "1111");
+        User currentUser = bibliotecaUser.getCurrentUser();
         // Then
         assertEquals("login should return user 111-1111", "111-1111", user.getId());
-        User currentUser = bibliotecaUser.getCurrentUser();
         assertEquals("Current user should same as the logged in user", user, currentUser);
+    }
 
-        // Negative Test
+    @Test
+    public void shouldNotLoginWrongUser() {
         // When
-        user = bibliotecaUser.login("xxx-xxxx", "xxxx");
+        User user = bibliotecaUser.login("xxx-xxxx", "xxxx");
+        User currentUser = bibliotecaUser.getCurrentUser();
         // Then
         assertEquals("login should return null", null, user);
-        currentUser = bibliotecaUser.getCurrentUser();
         assertEquals("Current user should be null", null, currentUser);
     }
 
     @Test
-    public void logoutTest() {
-        // Positive Test
-        // Given
-        User user = bibliotecaUser.login("111-1111", "1111");
-        // When
-        User loggedOutUser = bibliotecaUser.logout();
-        // Then
-        assertEquals("logout should return user 111-1111", "111-1111", loggedOutUser.getId());
-        User currentUser = bibliotecaUser.getCurrentUser();
-        assertEquals("Current user should be null", null, currentUser);
-
-        // Negative Test
+    public void shouldLogoutWhenHaveUser() {
         // Given
         bibliotecaUser.login("111-1111", "1111");
-        bibliotecaUser.logout();
         // When
-        loggedOutUser = bibliotecaUser.logout();
+        User loggedOutUser = bibliotecaUser.logout();
+        User currentUser = bibliotecaUser.getCurrentUser();
+        // Then
+        assertEquals("logout should return user 111-1111", "111-1111", loggedOutUser.getId());
+        assertEquals("Current user should be null", null, currentUser);
+    }
+
+
+    @Test
+    public void shouldNotLogoutWhenNoUser() {
+        // When
+        User loggedOutUser = bibliotecaUser.logout();
+        User currentUser = bibliotecaUser.getCurrentUser();
         // Then
         assertEquals("logout should return null if no current user", null, loggedOutUser);
-        currentUser = bibliotecaUser.getCurrentUser();
         assertEquals("Current user should be null", null, currentUser);
     }
 }
